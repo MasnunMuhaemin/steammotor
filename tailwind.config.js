7f7cb94c724bfbef059aead1594d36548168fb5b
MrFatra <ramadhanfatra2004@gmail.com>
2024-02-23 20:35:12 +0700

    chore:configure tailwind min-h & min-w

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      minHeight: {
+        '1/2': '1/2',
+      },
+      minWidth: {
+        '1/2': '1/2',
+      }
+    },
   },
   plugins: [],
 }
